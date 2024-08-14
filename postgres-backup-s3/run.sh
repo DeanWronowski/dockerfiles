46bf3efa67bc4113f3f1567fad7acb9f833fe403
Dean Wronowski <dean@deanwronowski.co.uk>
2024-08-14 21:17:07 +0100

    Tweaks

--- postgres-backup-s3/run.sh
+++ postgres-backup-s3/run.sh
@@ -9,6 +9,7 @@ fi
 if [ "${SCHEDULE}" = "**None**" ]; then
     sh backup.sh
 else
-    echo -e "SHELL=/bin/sh\n${SCHEDULE} root /bin/sh /backup.sh" > /etc/crontabs/root
-    exec go-crond /etc/crontabs/root
+    echo -e "SHELL=/bin/sh\n${SCHEDULE} /bin/sh /backup.sh" > /etc/crontabs/root
+    # Start dcron in the foreground
+    crond -f -l 2
 fi
