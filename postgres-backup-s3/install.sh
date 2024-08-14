f5b2fd8b9cd2c2f4875e1e5b98cfd605e2529691
Dean Wronowski <dean@deanwronowski.co.uk>
2024-08-14 21:21:24 +0100

    Tweaks

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -5,11 +5,5 @@ set -eo pipefail
 
 apk update
 
-# Install necessary packages
-apk add --no-cache openssl aws-cli postgresql-client --repository=https://dl-cdn.alpinelinux.org/alpine/v3.18/main
-
-# Install dcron or busybox-cron for cron functionality
-apk add --no-cache dcron  # Alternatively, use busybox-cron
-
 # Cleanup
 rm -rf /var/cache/apk/*
