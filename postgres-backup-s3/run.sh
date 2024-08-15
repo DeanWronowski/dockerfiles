77e6dc4ef2cb8b519d4f2f79008d35a2c30a8576
Dean Wronowski <dean@deanwronowski.co.uk>
2024-08-15 13:24:34 +0100

    Tweak

--- postgres-backup-s3/run.sh
+++ postgres-backup-s3/run.sh
@@ -8,7 +8,7 @@ if [ "${SCHEDULE}" = "**None**" ]; then
     sh backup.sh
 else
     # Debian uses /etc/cron.d for cron jobs instead of /etc/crontabs/root
-    echo -e "${SCHEDULE} /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
+    echo "${SCHEDULE} /backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
     chmod 0644 /etc/cron.d/backup-cron  # Ensure the correct permissions
     crontab /etc/cron.d/backup-cron  # Install the cron job
     cron -f  # Start cron in the foreground
