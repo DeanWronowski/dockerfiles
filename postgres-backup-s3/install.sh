888e44db7ecaa7b5b43ee1fc83b6bc0c214ffc1f
Dean Wronowski <dean@deanwronowski.co.uk>
2024-08-15 12:11:23 +0100

    Correct url

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 apk update
 
-wget https://github.com/webdevops/go-crond/releases/download/vx.x.x/go-crond_linux_amd64.tar.gz
+wget https://github.com/webdevops/go-crond/releases/download/23.12.0/go-crond_linux_amd64.tar.gz
 tar -xzvf go-crond_linux_amd64.tar.gz
 sudo mv go-crond /usr/local/bin/
 
