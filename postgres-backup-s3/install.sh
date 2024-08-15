7dc0ce1330cc9034f3fa0b0e36a263207c4d651d
Dean Wronowski <dean@deanwronowski.co.uk>
2024-08-15 11:41:20 +0100

    Tweaks

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -5,5 +5,10 @@ set -eo pipefail
 
 apk update
 
+wget https://github.com/webdevops/go-crond/releases/download/vx.x.x/go-crond_linux_amd64.tar.gz
+tar -xzvf go-crond_linux_amd64.tar.gz
+sudo mv go-crond /usr/local/bin/
+
+
 # Cleanup
 rm -rf /var/cache/apk/*
